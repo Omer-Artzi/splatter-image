9641fdef6cdb95e2197ffe9eecc57c9b8cf03dde
Omer Artzi <omerartzi5@gmail.com>
2024-09-06 11:15:25 +0300

    Remove error causing lines

--- gradio_app.py
+++ gradio_app.py
@@ -30,7 +30,6 @@ from huggingface_hub import hf_hub_download
 @torch.no_grad()
 def main():
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
     device = torch.device("cuda:0")
     torch.cuda.set_device(device)
 
@@ -224,7 +223,7 @@ def main():
         )
 
     demo.queue(max_size=1)
-    demo.launch()
+    demo.launch(share=True)
 
 if __name__ == "__main__":
     main()
